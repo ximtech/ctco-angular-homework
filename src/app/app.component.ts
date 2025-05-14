4b74e06e6d75221a417e25db6acf89da60cf1fe8
Alexander Baluhin <baluhins@gmail.com>
2025-05-14 18:12:06 +0300

    Migrate project to Angular 19, use Control Flow and Standalone Components (#3)
    * chore: exclude .idea from git
    
    * deps: update to Angular 17
    
    * deps: update to Angular 18
    
    * deps: update Angular ESLint
    
    * deps: update to Angular 19
    
    * feat: move to standalone components
    
    * feat: move to control flow
    
    * deps: use latest typescript-eslint plugins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
+
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-template';

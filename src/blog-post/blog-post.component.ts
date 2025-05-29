3b68a9988de3bcc74d0a41f861650ec757db4b1c
Stanislav Vodolagin <ximtech@inbox.lv>
2025-05-29 22:23:37 +0300

    Comments display view update

--- src/blog-post/blog-post.component.ts
+++ src/blog-post/blog-post.component.ts
@@ -1,11 +1,11 @@
-import {AfterViewInit, Component, computed, effect, inject, signal} from '@angular/core';
+import {AfterViewInit, Component, inject, OnInit, signal} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {FormsModule} from '@angular/forms';
 import {AsyncPipe, NgClass} from '@angular/common';
 import {BlogHeaderComponent} from '../blog-header/blog-header.component';
 import {Popover} from 'bootstrap';
 import {BlogApiService} from '../shared/service/blog-api.service';
-import {map, Observable} from 'rxjs';
+import {Observable} from 'rxjs';
 import {IBlogPostItem} from '../shared/model/blog-post.model';
 import {BlogPostCommentModel} from '../shared/model/blog-post-comment.model';
 import {IBlogPostAuthor} from '../shared/model/blog-post-author.model';
@@ -21,15 +21,13 @@ import {IBlogPostAuthor} from '../shared/model/blog-post-author.model';
   templateUrl: './blog-post.component.html',
   styleUrl: './blog-post.component.css'
 })
-export class BlogPostComponent implements AfterViewInit {
+export class BlogPostComponent implements OnInit, AfterViewInit {
 
   private blogApiService: BlogApiService = inject(BlogApiService);
   private route: ActivatedRoute = inject(ActivatedRoute);
 
   postId: number = this.route.snapshot.params['id'];
   authorId: number = this.route.snapshot.params['userId'];
-  newComment = signal<BlogPostCommentModel | undefined>(undefined);
-
   currentRating: number = 0;
   hoverRating: number = 0;
 
@@ -39,38 +37,44 @@ export class BlogPostComponent implements AfterViewInit {
 
   blogPost = signal(this.blogPostData$);
   authorInfo = signal(this.blogPostAuthorInfo$);
-  postComments = computed(() => {
-    console.log(this.newComment());
-    return this.blogPostComments$.pipe(
-      map((comments:BlogPostCommentModel[]) => {
-        return comments;
-      }));
-  });
+  postComments = signal<BlogPostCommentModel[]>([]);
+
+  ngOnInit(): void {
+    this.postComments.update((items: BlogPostCommentModel[]) => {
+      this.blogPostComments$.subscribe((values: BlogPostCommentModel[]) => Array.prototype.push.apply(items, values));
+      return items;
+    });
+  }
 
   ngAfterViewInit(): void {
     const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
     popoverTriggerList.forEach(el => new Popover(el, { trigger: 'focus', placement: 'top' }));
   }
 
-  submitComment(formValue: any) {
-    this.newComment.set({
-      email: formValue.name,
-      name: formValue.name,
-      postId: this.postId,
-      body: formValue.message} as BlogPostCommentModel);
-    this.blogApiService.saveNewComment(this.newComment())
-      .subscribe(value => console.log(value));
+  submitComment(formValue: any): void {
+    this.postComments.update((items: BlogPostCommentModel[]) => {
+      const newComment = {
+        email: formValue.name,
+        name: formValue.name,
+        postId: this.postId,
+        avatarImageUrl: 'https://avatar.iran.liara.run/public',
+        body: formValue.message} as BlogPostCommentModel
+
+      this.blogApiService.saveNewComment(newComment)
+        .subscribe(value => console.log(value));
+      return [...items, newComment] as BlogPostCommentModel[]
+    });
   }
 
-  setRating(rating: number) {
+  setRating(rating: number): void {
     this.currentRating = rating;
   }
 
-  setHover(rating: number) {
+  setHover(rating: number): void {
     this.hoverRating = rating;
   }
 
-  clearHover() {
+  clearHover(): void {
     this.hoverRating = 0;
   }
 
